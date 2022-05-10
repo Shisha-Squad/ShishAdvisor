89c82fc69ac23415735eb1c9d9087aa4a092e79a
meabefir <hujapetru@yahoo.com>
2022-05-11 00:11:10 +0300

    added button and text components

--- ShishAdvisor/App.js
+++ ShishAdvisor/App.js
@@ -3,7 +3,23 @@ import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
+import AppText from './app/components/AppText';
+import AppButton from './app/components/AppButton';
+
+import {MaterialCommunityIcons} from '@expo/vector-icons';
+import { Fragment } from 'react/cjs/react.development';
+
+
 export default function App() {
-    return <WelcomeScreen/>; 
-    // return <ViewImageScreen/>;
+    return (
+        <Fragment>
+            <WelcomeScreen/>
+            {/* <MaterialCommunityIcons name="email" /> */}
+            {/* <AppText>tst</AppText> */}
+            {/* <ViewImageScreen/> */}
+            <AppButton title="login" onPress={() => console.log("tapped")}>
+
+            </AppButton>
+        </Fragment>
+    ); 
 }
