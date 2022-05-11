53783bade1e02e8f3a80b082ad8edfd9e9298e71
meabefir <hujapetru@yahoo.com>
2022-05-11 16:47:28 +0300

    list item comp and view image icons

--- ShishAdvisor/App.js
+++ ShishAdvisor/App.js
@@ -8,16 +8,20 @@ import Card from './app/components/Card';
 
 import {MaterialCommunityIcons} from '@expo/vector-icons';
 import { Fragment } from 'react/cjs/react.development';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
     return (
         <Fragment>
-            <WelcomeScreen/>
+            {/* <WelcomeScreen/> */}
+            
             {/* <MaterialCommunityIcons name="email" /> */}
             {/* <AppText>tst</AppText> */}
-            {/* <ViewImageScreen/> */}
+            <ViewImageScreen/>
             {/* <AppButton title="login" onPress={() => console.log("tapped")} /> */}
-            <Card title="test title" subtitle="100$" image={require('./app/assets/places/whys1.jpg')}/>
+            {/* <Card title="test title" subtitle="100$" image={require('./app/assets/places/whys1.jpg')}/> */}
+
+            {/* <ListingDetailsScreen /> */}
         </Fragment>
     ); 
 }
