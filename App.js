2e91b95aa5ef77016cd56b7f86af492287fbb3a0
matei <matei.neagu19@yahoo.com>
2022-06-12 13:10:20 +0300

    try out different navigations

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Button } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ListingEditScreen from "./app/screens/ListingEditScreen";
@@ -25,14 +25,76 @@ import * as Permissions from 'expo-permissions';
 import ImageInput from './app/components/ImageInput';
 import ImageInputList from './app/components/ImageInputList';
 
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 const categories = [
     {label: "Love66", value: 1},
     {label: "Niculae", value: 2},
     {label: "Apple", value: 3},
 ]
 
+const Link = () => {
+    const navigation = useNavigation();
+    return (
+        <Button
+        title='Click'
+        onPress={() => navigation.navigate('TweetDetails', {id: 1})} />
+    );
+}
+const Tweets = ({navigation}) => (
+    <Screen>
+        <Text>Tweets</Text>
+        <Link/>
+    </Screen>
+)
+const TweetDetails = ({route}) => (
+    <Screen>
+        <Text>Tweet details {route.params.id} </Text>
+    </Screen>
+)
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+    <Stack.Navigator
+        screenOptions={{
+            headerStyle: { backgroundColor: "dodgerblue"},
+                headerTintColor: "white",
+                headerShown: true,
+        }}>
+        <Stack.Screen name="Tweets" component={Tweets}/>
+        <Stack.Screen name="TweetDetails" component={TweetDetails}
+            options={({route}) => ({ title: route.params.id})}/>
+    </Stack.Navigator>
+)
+
+const Account = () => <Screen><Text>Account</Text></Screen>
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+    <Tab.Navigator
+        screenOptions = {{
+            activeBackgroundColor: 'blue',
+            activeTintColor: 'white',
+            inactiveBackgroundColor: '#eee',
+            inactiveTintColor: "black"
+        }}>
+        <Tab.Screen name="Feed" component={StackNavigator}
+            options={{
+              tabBarIcon: ({ size, color }) => <MaterialCommunityIcons name="home" size={size} color={color}/>  
+            }}/>
+        <Tab.Screen name="Account" component={Account}/>
+    </Tab.Navigator>
+)
 export default function App() {
-    return <ListingEditScreen/>;
+    return (
+        <NavigationContainer>
+            <TabNavigator/>
+        </NavigationContainer>
+    );
+    // return <ListingEditScreen/>;
+
+
     // const [imageUris, setImageUris] = useState([]);
 
     // const handleAdd = uri => {
